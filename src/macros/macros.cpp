8f765a8a02526b34a3e9dddb2ab9c7d5e09c2380
Zach Champion <zachchampion79@gmail.com>
2018-01-17 09:33:18 -0500

    stopMacro() now enables all driver control

--- src/macros/macros.cpp
+++ src/macros/macros.cpp
@@ -1,13 +1,25 @@
 #include "macros.h"
 #include "io_control.h"
+#include "driver_control.h"
 
 TaskHandle task_macros;
 unsigned char current_macro = NONE;
 
 // kills the macro task and starts a blank one
 void stopMacro() {
+  
+  // kill the task
   taskDelete(task_macros);
+  
+  // reset varaibles for raw driver control
   current_macro = NONE;
+  driver_chassis = true;
+  driver_lift = true;
+  driver_claw = true;
+  driver_mogo = true;
+  driver_chainbar = true;
+  
+  // restart the task
   macrosStartListener();
 }
 
