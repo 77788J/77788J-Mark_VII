b20524ca1d91bbab504b10d85a1658d366cb7685
ZachDaChampion <zachchampion79@gmail.com>
2018-02-24 22:38:59 -0500

    added documentation to AP Filter Inputs git push 

--- src/lib/ap_filter_input.cpp
+++ src/lib/ap_filter_input.cpp
@@ -1,6 +1,17 @@
 #include "ap_filter.h"
 #include "math.h"
 
+// this object is used as an input for the AP filter
+// reliability is an array of size AP_FILTER_MAX_INPUT
+// the array is used to determine the baseline reliability of a sensor
+// reliability is a polynomial, with each element in the array being a coefficent
+// for example, 2x^2 + 5x - 1 would be {2, 5, -1}
+// reliability_count stores how many elements should be used
+// the array technically has AP_FILTER_MAX_INPUT entries, so it needs to know when to stop
+// often, reliability_count will be 1 and the array will have only 1 element
+// this would effectively produce a constant weight
+// while the filter works with constant weights, the polynomial makes it MUCH beter, especially for gyros
+
 ApFilterInput :: ApFilterInput() {}
 
 void ApFilterInput :: init(int count_, float *reliability_) {
